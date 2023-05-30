6593c2213eb61a1121136797da3b7942fdc8b41c
George Hill <georgehill@Georges-MacBook-Pro.local>
2023-05-30 15:08:17 +0200

    importing route functions

--- routes/deckRouter.js
+++ routes/deckRouter.js
@@ -1,2 +1,11 @@
 const express = require ('express') ;
 
+const {
+    index,
+    show,
+    create,
+    update,
+    destroy
+    } = require ('../controllers/deckController.js') ;
+
+const router = express.Router();
