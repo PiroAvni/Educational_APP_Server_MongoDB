e693149b2b19c806e1553a0c3e98b032956f5381
PiroAvni <avnipiro.dev@gmail.com>
2023-05-31 19:13:55 +0100

    add test and endpoint

---
+++ __test__/setupTest.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "@shelf/jest-mongodb",
+    // testEnvironment: ‘node’,
+    // testMatch: [‘**/__tests__/**/*.js’, ‘**/?(*.)+(spec|test).js’],
+    // setupFilesAfterEnv: [‘./__test__/setupTest.js’],
+    // coverageDirectory: ‘./coverage’,
+    // collectCoverage: true,
+    // collectCoverageFrom: [‘src/**/*.js’],
+  }
