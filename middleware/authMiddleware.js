4ae536e851266c9cf110c61a5d64263b75ef8b7b
PiroAvni <avnipiro.dev@gmail.com>
2023-05-31 09:34:15 +0100

    add testing

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require ('jsonwebtoken') ;
 const asyncHandler = require ('express-async-handler') ;
-const User = require ('../models/Users.js') ;
+
+const User = require ('../models/Users.js')
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
