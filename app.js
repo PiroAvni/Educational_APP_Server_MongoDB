64a1883953961853da84c9744cde8577ef4f355d
PiroAvni <avnipiro.dev@gmail.com>
2023-06-01 18:09:35 +0100

    add cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: 'https://educational-app.onrender.com' }));
 app.use(logger)
 
 app.use('/api/users', userRoutes)
